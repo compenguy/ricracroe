f1eb8f74a94e36331dbbb558418c539d2aa394f8
Will Page <compenguy@gmail.com>
2017-01-18 19:26:05 -0800

    Adding display support for game state
    Adding display formatting for cell and board so that
    we can display the state of the board easily.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+// Allow default debug output display
+#[derive(Debug)]
 // Allow us to do equality tests on enum members
-// Give it copy semantics
 #[derive(PartialEq)]
+// Give it copy semantics
 #[derive(Clone, Copy)]
 enum RRRCell {
     Clear,
@@ -8,13 +10,90 @@ enum RRRCell {
     O,
 }
 
+impl std::fmt::Display for RRRCell {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            RRRCell::Clear => write!(f, "·"),
+            RRRCell::X     => write!(f, "X"),
+            RRRCell::O     => write!(f, "O"),
+        }
+    }
+}
+
+#[derive(Debug)]
 struct RRRBoard {
     cells: std::collections::HashMap<(u32, u32),RRRCell>,
     xsize: u32,
     ysize: u32,
 }
 
+// Box drawing chars from https://en.wikipedia.org/wiki/Box-drawing_character
+impl std::fmt::Display for RRRBoard {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let xsize = self.get_xsize();
+        let ysize = self.get_ysize();
+
+        for y in 0..ysize {
+            try!(write!(f, "  "));
+            if y == 0 {
+                // Display the top border
+                for x in 0..xsize {
+                    if x == 0 { try!(write!(f, "╭")); }
+                    else      { try!(write!(f, "┬")); }
+                    try!(write!(f, "─"));
+                }
+                try!(write!(f, "╮"));
+            } else {
+                // Display interstitial borders
+                for x in 0..xsize {
+                    if x == 0 { try!(write!(f, "├")); }
+                    else      { try!(write!(f, "┼")); }
+                    try!(write!(f, "─"));
+                }
+                try!(write!(f, "┤"));
+            }
+            try!(write!(f, "\n"));
+
+            // Display one row of board cells
+            try!(write!(f, "{} ", y));
+            for x in 0..xsize {
+                try!(write!(f, "│"));
+                // TODO: Come back and add error handling
+                try!(write!(f, "{}", self.fetch(x, y).unwrap()));
+            }
+            try!(write!(f, "│"));
+            try!(write!(f, "\n"));
+        }
+
+        // Display the bottom border
+        try!(write!(f, "  "));
+        for x in 0..xsize {
+            if x == 0 { try!(write!(f, "╰")); }
+            else      { try!(write!(f, "┴")); }
+            try!(write!(f, "─"));
+        }
+        try!(write!(f, "╯"));
+        try!(write!(f, "\n"));
+
+        try!(write!(f, "  "));
+        for x in 0..xsize {
+            if x == 0 { try!(write!(f, " ")); }
+            else      { try!(write!(f, " ")); }
+            try!(write!(f, "{}", x));
+        }
+        write!(f, "\n")
+    }
+}
+
 impl RRRBoard {
+    fn get_xsize(&self) -> u32 {
+        self.xsize
+    }
+
+    fn get_ysize(&self) -> u32 {
+        self.ysize
+    }
+
     fn fetch(&self, x: u32, y: u32) -> Result<RRRCell, &'static str> {
         match self.cells.get(&(x, y)) {
             Some(cell) => Ok(*cell),
@@ -73,6 +152,17 @@ impl RRRBoard {
 
 fn main() {
     let mut board = RRRBoard::new();
-    // Right now, just being lazy and allowing errors to panic
+    // TODO: Right now, just being lazy and allowing errors to panic
+    println!("Starting board:\n{}", board);
     board.make_move(0, 0, RRRCell::X).unwrap();
+    println!("After single move:\n{}", board);
+    board.make_move(0, 1, RRRCell::O).unwrap();
+    println!("After next move:\n{}", board);
+    board.make_move(2, 2, RRRCell::X).unwrap();
+    println!("After next move:\n{}", board);
+    match board.make_move(2, 2, RRRCell::O) {
+        Ok(_) => println!("Well, that worked."),
+        Err(e) => println!("Something went wrong: {}", e),
+    }
+    println!("After next move:\n{}", board);
 }
